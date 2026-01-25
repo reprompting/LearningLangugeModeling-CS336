9c408d279fa2d77236773adfb4a7b22e85aa62d7
reprompting <245037217+reprompting@users.noreply.github.com>
2026-01-25 23:47:03 +0100

    added a linear layer today

---
+++ assignment1-basics/transformer.py
@@ -0,0 +1,24 @@
+import torch 
+import torch.nn as nn 
+import math 
+
+class LinearLayer(nn.Module):
+    def __init__(self, in_feature, out_feature, device=None, dtype=None):
+        super().__init__()
+
+        self.in_features = in_feature 
+        self.out_features = out_feature 
+        self.weight = nn.Parameter(torch.empty(out_feature, in_feature))
+        std = math.sqrt( 2 / (in_feature + out_feature))
+        nn.init.trunc_normal_(self.weight, std=std)
+
+    def forward(self, x):
+        return ((x@self.weight.T))
+    
+if __name__ == "__main__":
+    print("hello")
+    model  = LinearLayer(2,3)
+    data= torch.randn(12,2)
+    output = model(data)
+    print(output, "/n")
+
